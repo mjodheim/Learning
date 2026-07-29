bb680813bf3a5e375afe8edefa476a296acc5d88
Anthony Mets <110968830+mjodheim@users.noreply.github.com>
2026-07-29 11:47:47 +0200

    fix(java): corriger la somme des lignes de matrice

--- Java/Basics/src/Matrices/SommeMatrice3x3.java
+++ Java/Basics/src/Matrices/SommeMatrice3x3.java
@@ -2,29 +2,29 @@ package Matrices;
 
 import java.util.Random;
 
-public class SommeMatrice3x3 {
+public final class SommeMatrice3x3 {
+    private SommeMatrice3x3() {
+    }
+
     public static void run() {
         int[][] matrice = new int[3][3];
-        int[] result = new int[3];
-        StringBuilder matriceString = new StringBuilder();
-
+        int[] sommes = new int[3];
         Random random = new Random();
 
-        // On remplit la matrice avec des nombres aléatoires de 0 à 9 et on les affiche.
-        // On récupère en même temps la somme de chaque ligne.
-
         for (int i = 0; i < matrice.length; i++) {
+            StringBuilder ligne = new StringBuilder("| ");
+
             for (int j = 0; j < matrice[i].length; j++) {
                 matrice[i][j] = random.nextInt(10);
-                matriceString.append(matrice[i][j]).append(" | ");
-                result[i] += matrice[i][j];
+                sommes[i] += matrice[i][j];
+                ligne.append(matrice[i][j]).append(" | ");
             }
-            System.out.println(matriceString);
+
+            System.out.println(ligne);
         }
 
-        // Affichage du résultat de chaque ligne matricielle.
-        for (int element : result) {
-            System.out.println("Ligne 1: " + result[element]);
+        for (int i = 0; i < sommes.length; i++) {
+            System.out.printf("Somme de la ligne %d : %d%n", i + 1, sommes[i]);
         }
     }
 }
