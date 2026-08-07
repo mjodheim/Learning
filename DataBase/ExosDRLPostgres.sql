1c1a8d9f089632eb6e3c2c20679afce22d032603
Anthony Mets <110968830+mjodheim@users.noreply.github.com>
2026-08-07 11:48:53 +0200

    Fin des exercices DRL avec jointures.

--- DataBase/ExosDRLPostgres.sql
+++ DataBase/ExosDRLPostgres.sql
@@ -283,7 +283,7 @@ GROUP BY "section_id";
 -- PRÉCIS (dans une colonne appelée « Moyenne ») obtenu par les étudiants
 
 SELECT "section_id",
-    AVG("year_result") AS "Moyenne"
+    CAST(AVG("year_result") AS INT) AS "Moyenne"
 FROM "student"
 WHERE CAST("section_id" AS VARCHAR(10)) LIKE '10%'
 GROUP BY "section_id";
@@ -293,7 +293,7 @@ GROUP BY "section_id";
 -- entre 1970 et 1985
 
 SELECT EXTRACT(MONTH FROM "birth_date") AS "Mois de naissance",
-    CAST(AVG("year_result") AS INTEGER) AS "Moyenne"
+    CAST(AVG("year_result") AS INT) AS "Moyenne"
 FROM "student"
 WHERE EXTRACT(YEAR FROM "birth_date") BETWEEN 1970 AND 1985
 GROUP BY EXTRACT(MONTH FROM "birth_date");
@@ -302,7 +302,7 @@ GROUP BY EXTRACT(MONTH FROM "birth_date");
 -- moyenne PRÉCISE des résultats annuels (dans une colonne appelée « Moyenne »)
 
 SELECT "section_id",
-    AVG("year_result") AS "Moyenne"
+    AVG("year_result")::DOUBLE PRECISION AS "Moyenne"
 FROM "student"
 GROUP BY "section_id"
 HAVING COUNT(*) > 3;
@@ -311,8 +311,117 @@ HAVING COUNT(*) > 3;
 -- sections dont le résultat moyen est supérieur à 8
 
 SELECT "section_id",
-    CAST(AVG("year_result") AS INTEGER) AS "Moyenne",
+    CAST(AVG("year_result") AS INT) AS "Moyenne",
     MAX("year_result") AS "Résultat maximum"
 FROM "student"
 GROUP BY "section_id"
 HAVING AVG("year_result") > 8;
+
+-- Exercice 2.6.1 – Donner pour chaque cours le nom du professeur responsable ainsi que la
+-- section dont le professeur fait partie
+
+SELECT 
+    c."course_name",
+    s."section_name",
+    p."professor_name"
+FROM "course" c
+    JOIN "professor" p ON c."professor_id" = p."professor_id"
+    JOIN "section" s ON p."section_id" = s."section_id";
+
+-- Exercice 2.6.2 – Donner pour chaque section, l’id, le nom et le nom de son délégué. Classer les
+-- sections dans l’ordre inverse des id de section. Un délégué est un étudiant de la table « STUDENT »
+
+SELECT 
+    s."section_id",
+    s."section_name",
+    st."last_name"
+FROM "section" s
+    LEFT JOIN "student" st ON s."delegate_id" = st."student_id"
+ORDER BY s."section_id" DESC;
+
+-- Exercice 2.6.3 – Donner pour chaque section, le nom des professeurs qui en sont membre
+
+SELECT 
+    s."section_id",
+    s."section_name",
+    p."professor_name"
+FROM "section" s
+    LEFT JOIN "professor" p ON s."section_id" = p."section_id"
+ORDER BY s."section_id" DESC;
+
+-- Exercice 2.6.4 – Même objectif que la question 3 mais seuls les sections comportant au moins
+-- un professeur doivent être reprises
+
+SELECT 
+    s."section_id",
+    s."section_name",
+    p."professor_name"
+FROM "section" s
+    JOIN "professor" p ON s."section_id" = p."section_id"
+ORDER BY s."section_id" DESC;
+
+-- Exercice 2.6.5 – Donner à chaque étudiant ayant obtenu un résultat annuel supérieur ou égal à
+-- 12 son grade en fonction de son résultat annuel et sur base de la table grade. La liste doit être
+-- classée dans l’ordre alphabétique des grades attribués
+
+SELECT 
+    st."last_name",
+    st."first_name",
+    g."grade"
+FROM "student" st
+    JOIN "grade" g ON st."year_result" BETWEEN g."lower_bound" AND g."upper_bound"
+WHERE st."year_result" >= 12
+ORDER BY g."grade" ASC;
+
+-- Exercice 2.6.6 – Donner la liste des professeurs et la section à laquelle ils se rapportent ainsi
+-- que le(s) cour(s) (nom du cours et crédits) dont le professeur est responsable. La liste est triée par
+-- ordre décroissant des crédits attribués à un cours
+
+SELECT
+    p."professor_name",
+    s."section_name",
+    c."course_name",
+    c."course_ects"
+FROM "section" s
+    JOIN "professor" p ON p."section_id" = s."section_id"
+    LEFT JOIN "course" c ON p."professor_id" = c."professor_id"
+ORDER BY c."course_ects" DESC;
+
+-- Exercice 2.6.7 – Donner pour chaque professeur son id et le total des crédits ECTS
+-- (« ECTS_TOT ») qui lui sont attribués. La liste proposée est triée par ordre décroissant de la somme
+-- des crédits alloués
+
+SELECT
+    p."professor_id",
+    SUM(c."course_ects") AS "ECTS_TOT"
+FROM "professor" p
+    LEFT JOIN "course" c ON p."professor_id" = c."professor_id"
+GROUP BY p."professor_id"
+ORDER BY "ECTS_TOT" DESC;
+
+-- Exercice 2.6.8 – Donner la liste (nom et prénom) de l’ensemble des professeurs et des
+-- étudiants dont le nom est composé de plus de 8 lettres. Ajouter une colonne pour préciser la
+-- catégorie (S pour « STUDENT », P pour « PROFESSOR ») à laquelle appartient l’individu
+
+SELECT 
+    st."first_name", 
+    st."last_name",
+    'S' AS "Catégorie"
+FROM "student" st
+WHERE LENGTH(st."last_name") > 8
+UNION
+SELECT 
+    p."professor_surname", 
+    p."professor_name",
+    'P' AS "Catégorie"
+FROM "professor" p
+WHERE LENGTH(p."professor_name") > 8
+ORDER BY "Catégorie";
+
+-- Exercice 2.6.9 – Donner l’id de chacune des sections qui n’ont pas de professeur attitré
+
+SELECT s."section_id"
+FROM "section" s
+    LEFT JOIN "professor" p ON s."section_id" = p."section_id"
+WHERE p."professor_id" IS NULL
+ORDER BY s."section_id";
