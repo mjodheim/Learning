fbed05033082b94c269ca4c3cbae3eb17ab8f9fc
Anthony Mets <110968830+mjodheim@users.noreply.github.com>
2026-09-10 12:35:59 +0200

    Spring intro: GetMapping, Params, templates

---
+++ Java/Spring/introSpringMVC/src/main/java/be/mjodheim/introspringmvc/controllers/HomeController.java
@@ -0,0 +1,39 @@
+package be.mjodheim.introspringmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String home(){
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+
+    @GetMapping("/say-hello/{name}")
+    public String hello(
+            @PathVariable String name,
+            Model model
+    ){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+
+    @GetMapping("/say-bye")
+    public String bye(
+            @RequestParam String name,
+            Model model
+    ){
+        model.addAttribute("name",name);
+        return "bye";
+    }
+}
